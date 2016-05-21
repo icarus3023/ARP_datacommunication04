eae858584d49635c5d866a15cb17ae50f9c3c396
hanareum95 <areum9502@gmail.com>
2016-05-21 19:58:25 +0900

    IPLayer수정본입니다. Table추가와 삭제 등이 있습니다.

--- ChattingFileTransfer_2/IPLayer.cpp
+++ ChattingFileTransfer_2/IPLayer.cpp
@@ -2,6 +2,7 @@
 //
 //////////////////////////////////////////////////////////////////////
 
+#include <WinSock2.h>
 #include "stdafx.h"
 #include "arp.h"
 #include "IPLayer.h"
@@ -19,6 +20,7 @@ static char THIS_FILE[]=__FILE__;
 CIPLayer::CIPLayer( char* pName )
 : CBaseLayer( pName )
 {
+	entrycount = 0;
 	ResetHeader( );
 }
 
@@ -79,4 +81,55 @@ BOOL CIPLayer::Receive(unsigned char* ppayload)
 		bSuccess = mp_aUpperLayer[0]->Receive((unsigned char*)pFrame->ip_data);
 	}
 	return bSuccess ;
+}
+
+// 라우터 테이블 아이템 추가
+void CIPLayer::addEntry(ROUTE_TABLE_ITEM &item) {
+	entry[entrycount++] = item;
+	RouterTablesort();
+}
+
+
+// 라우터 테이블 아이템 번호 반환
+int CIPLayer::getEntryCount() {
+	return entrycount;
+}
+
+// 아이템 삭제
+void CIPLayer::delEntry(int &index) {
+	int i;
+	for (i = index; i < entrycount; i++) {
+		entry[i] = entry[i + 1];
+	}
+	entrycount--;
+}
+
+// 아이템 주소값 반환
+ROUTE_TABLE_ITEM* CIPLayer::getEntry(int &index) {
+	return &entry[index];
+}
+
+
+// 라우터 테이블의 아이템 정렬(=내림차순)
+void CIPLayer::RouterTablesort() {
+	int i;
+	bool flag = true;
+	ROUTE_TABLE_ITEM temp;
+
+	while (flag) {
+		flag = false;
+		for (i = 0; i < entrycount - 1; i++) {
+			unsigned long net1, net2;
+			memcpy(&net1, entry[i].netmask.s_ip_addr, 4);
+			memcpy(&net2, entry[i + 1].netmask.s_ip_addr, 4);
+			// 내림차순 정렬
+			if (net1 < net2)
+			{
+				temp = entry[i];
+				entry[i] = entry[i + 1];
+				entry[i + 1] = temp;
+				flag = true;
+			}
+		}
+	}
 }
