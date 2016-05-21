95f6ae2e3c588041a0a2abd0516c83344d9da3c5
hanareum95 <areum9502@gmail.com>
2016-05-21 19:57:28 +0900

    IPLayer.h수정본입니다.

--- ChattingFileTransfer_2/IPLayer.h
+++ ChattingFileTransfer_2/IPLayer.h
@@ -15,6 +15,34 @@
 // 경로방향 : 호스트
 #define HOST_FLAG 0x04
 
+// 라우터 테이블
+typedef struct ROUTE_TABLE_ITEM {
+	// 목적주소
+	IP_ADDR des;
+	// 마스크 범위
+	IP_ADDR netmask;
+	// 게이트웨이 주소
+	IP_ADDR gateway;
+	// flag : 경로의 상태 및 위치
+	char flag;
+	// 이더넷 카드 정보
+	int ethcard;
+	// 목적위치까지의 Hop 갯수
+	int metric;
+}ROUTE_TABLE_ITEM;
+
+
+typedef struct _IP_ADDR
+{
+	union {
+		struct { UCHAR s_b1, s_b2, s_b3, s_b4; } S_un_b;
+		struct { unsigned short s_w1, s_w2; } S_un_w;
+		unsigned long S_addr;
+		UCHAR s_ip_addr[4];
+	}S_un;
+
+} IP_ADDR, *LPIP_ADDR;
+
 class CIPLayer 
 : public CBaseLayer  
 {
@@ -22,6 +50,9 @@ private:
 	inline void		ResetHeader( );
 	unsigned char srcip[4];
 	unsigned char destip[4];
+	ROUTE_TABLE_ITEM entry[MAX_ENTRY];
+	int entrycount;
+	void RouterTablesort();
 
 public:
 	CIPLayer( char* pName );
@@ -29,6 +60,11 @@ public:
 	void SetSrcIPAddress(unsigned char* src_ip);
 	void SetDstIPAddress(unsigned char* dst_ip);
 	void SetFragOff(unsigned short fragoff);
+	ROUTE_TABLE_ITEM* getEntry(int &index);
+	void delEntry(int &index);
+	void addEntry(ROUTE_TABLE_ITEM &item);
+	int getEntryCount();
+
 
 	BOOL Send(unsigned char* ppayload, int nlength);
 	BOOL Receive(unsigned char* ppayload);
@@ -53,22 +89,6 @@ public:
 		IP_ADDR				cache_gatewayaddr;
 		unsigned short		cache_flag;
 	} STATIC_CACHE;
-
-	// 라우터 테이블
-	typedef struct ROUTE_TABLE_ITEM {
-		// 목적주소
-		IP_ADDR des;
-		// 마스크 범위
-		IP_ADDR netmask;
-		// 게이트웨이 주소
-		IP_ADDR gateway;
-		// flag : 경로의 상태 및 위치
-		char flag;
-		// 이더넷 카드 정보
-		int ethcard;
-		// 목적위치까지의 Hop 갯수
-		int metric;
-	}ROUTE_TABLE_ITEM;
 
 
 	CArray<STATIC_CACHE> static_table;
